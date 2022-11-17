7c83ce74bc3b32512ec228954e86c0406bf22da8
Timothy Pratley <timothypratley@gmail.com>
2022-11-16 21:26:31 -0800

    better looking spider

--- spider.js
+++ spider.js
@@ -1,26 +1,37 @@
 var spiderLegSockets =
-  [[-3,-1], [3,-1],
-   [-2,-2], [2,-2],
-   [-2,2], [2,2],
-   [-3,1], [3,1]];
+  [[0,-2], [3,-1],
+   [0.5,-1.5], [1,-1.5],
+   [0.5,1.5], [1,1.5],
+   [0,2], [3,1]];
+
+function hiccup(tag, attributes) {
+  var e = document.createElementNS("http://www.w3.org/2000/svg", tag);
+  for (k in attributes) {
+    e.setAttribute(k, attributes[k]);
+  }
+  return e;
+}
 
 function spiderLeg(socket) {
-  var l = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  var tr = math.multiply(socket, 3);
+  if (socket[1]>0) {
+    tr = math.add(tr, [-5,0]);
+  }
+  var l = hiccup("path", {d: "M"+socket+" L"+tr});
   l.socket = socket;
   l.down = true;
-  l.tr = math.multiply(socket, 3);
-  l.setAttribute("d", "M"+socket+" L"+l.tr);
+  l.tr = tr;
   return l;
 }
 
 function spider() {
-  var s = document.createElementNS("http://www.w3.org/2000/svg", "g");
-  s.setAttribute("stroke", "black")
-  var body = document.createElementNS("http://www.w3.org/2000/svg", "ellipse");
-  body.setAttribute("rx", 3);
-  body.setAttribute("ry", 2);
-  s.appendChild(body);
+  var s = hiccup("g", {stroke: "black"});
+  var bodyFront = hiccup("ellipse", {cx: 2, rx: 2, ry: 1});
+  s.appendChild(bodyFront);
+  var bodyBack = hiccup("ellipse", {cx: -2, rx: 3, ry: 2});
+  s.appendChild(bodyBack);
   s.tr = [0,0];
+//  s.goal = [0];
   s.rot = [0];
   s.v = [0,1];
   for (socket of spiderLegSockets) {
@@ -30,19 +41,22 @@ function spider() {
 }
 
 function updateSpider(s, dt) {
+//  if (Math.random() < 0.001) {
+//    s.rot = [Math.random()*360];
+//  }
   s.tr = math.add(s.tr, [dt, 0]);
-  s.setAttribute("transform", "translate("+s.tr+")");
+  s.setAttribute("transform", "translate("+s.tr+") rotate("+s.rot+")");
   for (leg of s.children) {
     if (leg.tr) {
       if (leg.down) {
         leg.tr = math.add(leg.tr, [-dt, 0]);
       } else {
-        leg.tr = math.add(leg.tr, [dt, 0]);
+        leg.tr = math.add(leg.tr, [4*dt, 0]);
       }
       leg.setAttribute("d", "M"+leg.socket+" L"+leg.tr);
-      if (leg.tr[0] > (leg.socket[0] > 0 ? 9 : 0)) {
+      if (leg.tr[0] > (leg.socket[0] > 0.5 ? 9 : -5)) {
         leg.down = true;
-      } else if (leg.tr[0] < (leg.socket[0] > 0 ? 0 : -9)) {
+      } else if (leg.tr[0] < (leg.socket[0] > 0.5 ? 5 : -9)) {
         leg.down = false;
       }
     }
@@ -54,7 +68,7 @@ var world = document.getElementById("world");
 var lastTs = performance.now();
 
 function animate(ts) {
-  var dt = (ts - lastTs)/1000;
+  var dt = (ts - lastTs)/100;
   lastTs = ts;
   for (child of world.children) {
     updateSpider(child, dt);
